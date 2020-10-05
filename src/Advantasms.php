58c0d7279ccd68adc912a84f5e6344bb99ec43c5
Sam Arossi Maosa <smaosa@strathmore.edu>
2020-10-05 18:23:33 +0300

    Fixed missing shortcode

--- src/Advantasms.php
+++ src/Advantasms.php
@@ -19,7 +19,7 @@ class Advantasms
     {
         $this->apikey = $apiKey;
         $this->partnerId = $partnerId;
-        $this->shortCode = $shortCode;
+        $this->shortcode = $shortCode;
         return $this;
     }
 
@@ -66,7 +66,43 @@ class Advantasms
             "mobile"=>trim($this->to)
         ];
         $response = $this->curlPost($this->sendsms,$data);
-        return $response;
+        /*
+            200;Successful Request Call
+            1001;Invalid sender id
+            1002;Network not allowed
+            1003;Invalid mobile number
+            1004;Low bulk credits
+            1005;Failed. System error
+            1006;Invalid credentials
+            1007;Failed. System error
+            1008;No Delivery Report
+            1009;unsupported data type
+            1010;unsupported request type
+            4090;Internal Error. Try again after 5 minutes
+            4091;No Partner ID is Set
+            4092;No API KEY Provided
+            4093;Details Not Found
+         */
+        $return = [
+            "success" => false,
+            "message" => "",
+            "payload" => []
+        ];
+        if (!$response) {
+            $return["success"] = false;
+            $return["message"] = "No response from the server.";
+        } else {
+            if (isset($response['response-code']) && $response["response-code"] === 200) {
+                $return["success"] = true;
+                $return["message"] = "Message sent successfully";
+                $return["payload"] = $response;
+            } else {
+                $return["success"] = false;
+                $return["message"] = $response["response-description"];
+                $return["payload"] = $response;
+            }
+        }
+        return $return;
     }
 
     /**
